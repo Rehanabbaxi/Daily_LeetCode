36fa6710603aff5fec9e4465784deadd2fded654
rehan ahmed <106952909+Rehanabbaxi@users.noreply.github.com>
2024-10-28 09:34:02 +0500

    Add files via upload
    Added Remove_Duplicates_from_Sorted_Array_II.java

---
+++ Solution_In_Java/Remove_Duplicates_from_Sorted_Array_II.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lc_solutions_in_java;
+
+
+public class Remove_Duplicates_from_Sorted_Array_II {
+    
+        public int removeDuplicates(int[] nums) {
+        int right ;
+        int left = 1 ;
+        int counter = 0 ;
+
+        for(right = 1 ; right < nums.length ; right++ ){
+            if(nums[right] == nums[right-1] ){
+                counter++;
+            }
+            else {
+                counter = 0;
+            }
+            if(counter < 2){
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
+        
+       public static void main(String[] args) {
+            Remove_Duplicates_from_Sorted_Array_II obj = new Remove_Duplicates_from_Sorted_Array_II();
+
+            int[] test_num = new int[]{0,0,1,1,1,1,2,3,3};
+            int result = obj.removeDuplicates(test_num);
+           System.out.println("Number of non dublicate elements is : " +result);
+    }
+}
+
